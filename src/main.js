261ea1576af8b5779b6f2b89cf33fd27e09ef45a
Ali Nooner <alinooner@gmail.com>
2022-01-17 13:17:33 -0600

    Add ability to display the winner

--- src/main.js
+++ src/main.js
@@ -7,13 +7,24 @@ var heading = document.getElementById('header');
 
 
 // EVENT LISTENERS
+// window.addEventListener('load', loadPage);
 
 // FUNCTIONS
 
 function updatePlayerTurnDisplay() {
   if (currentGame.turn === currentGame.player1 && currentGame.won === false) {
-    heading.innerText = 'It is 🐠 turn';
+    heading.innerText = "It's 🐠 's turn ";
   } else if (currentGame.turn === currentGame.player2 && currentGame.won === false) {
-    heading.innerText = 'It is 🧜🏼‍♀️ turn';
+    heading.innerText = "It's 🧜🏼‍♀️ 's turn";
+  }
+}
+
+function displayWinner() {
+  if (currentGame.won === true && currentGame.turn === currentGame.player1) {
+    heading.innerText = "🧜🏼‍♀️ won!";
+  } else if (currentGame.won === true && currentGame.turn === currentGame.player2) {
+    heading.innerText = "🐠 won!";
+  } else if (currentGame.won === false && currentGame.totalPlays === 9) {
+    heading.innerText = "It's a draw!";
   }
 }
